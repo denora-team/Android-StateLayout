6386271683733b3d57145affa5e6750a6b37583a
Moslem Deris <moslem.deris@gmail.com>
2019-02-18 19:49:05 +0330

    Show how to use the library in sample app

--- app/src/main/java/ir/denora/statelayout_sample/MainActivity.kt
+++ app/src/main/java/ir/denora/statelayout_sample/MainActivity.kt
@@ -1,12 +1,38 @@
 package ir.denora.statelayout_sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ir.denora.statelayout.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setListeners()
+    }
+
+    private fun setListeners() {
+        BTN_content.setOnClickListener { stateLayout.showContent() }
+        BTN_loading.setOnClickListener { stateLayout.showLoading() }
+        BTN_empty.setOnClickListener { stateLayout.showEmpty() }
+        BTN_notFound.setOnClickListener { stateLayout.showNotFound() }
+        BTN_noNetwork.setOnClickListener { stateLayout.showNoNetwork() }
+        BTN_error.setOnClickListener { stateLayout.showError() }
+
+        stateLayout.onStateChangedListener = object : OnStateChangedListener {
+            override fun onStateChanged(state: State) {
+                when (state) {
+                    ContentState -> TODO()
+                    LoadingState -> TODO()
+                    EmptyState -> TODO()
+                    NotFoundState -> TODO()
+                    NoNetworkState -> TODO()
+                    ErrorState -> TODO()
+                }
+            }
+        }
     }
 }
